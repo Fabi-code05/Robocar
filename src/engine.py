a91c665441e5728462ae1f2bbee2f67c5055aa88
Felix8346 <stuehlefs@gmail.com>
2026-05-13 16:49:18 +0200

    test front motors

--- src/engine.py
+++ src/engine.py
@@ -1,7 +1,7 @@
 import board
 from adafruit_pca9685 import PCA9685
-import timedatectl
 import logging
+import time
 
 
 log = logging.getLogger(__name__)
@@ -66,3 +66,56 @@ def rear_right(speed=0):
         log.error(f"speed {speed} outside of range 0-100")
         return
     
+    motor_speed = int((abs(speed) * 0xFFFF) / 100)
+    current_speed_front_left = speed
+    
+    if speed >= 0:
+        pca.channels[0].duty_cycle = 0
+        pca.channels[1].duty_cycle = motor_speed
+    if speed < 0:
+        pca.channels[0].duty_cycle = motor_speed
+        pca.channels[1].duty_cycle = 0
+        
+        
+def rear_left(speed=0):
+    if 0 > abs(speed) > 100:
+        log.error(f"speed {speed} outside of range 0-100")
+        return
+    
+    motor_speed = int((abs(speed) * 0xFFFF) / 100)
+    current_speed_front_left = speed
+    
+    if speed >= 0:
+        pca.channels[0].duty_cycle = 0
+        pca.channels[1].duty_cycle = motor_speed
+    if speed < 0:
+        pca.channels[0].duty_cycle = motor_speed
+        pca.channels[1].duty_cycle = 0
+        
+def front_right(speed=0):
+    if 0 > abs(speed) > 100:
+        log.error(f"speed {speed} outside of range 0-100")
+        return
+    
+    motor_speed = int((abs(speed) * 0xFFFF) / 100)
+    current_speed_front_left = speed
+    
+    if speed >= 0:
+        pca.channels[6].duty_cycle = 0
+        pca.channels[7].duty_cycle = motor_speed
+    if speed < 0:
+        pca.channels[6].duty_cycle = motor_speed
+        pca.channels[7].duty_cycle = 0
+        
+        
+if  __name__=="__main__":
+    
+    
+    
+    init()
+
+    front_left(0)
+    front_right(100)
+    
+    time.sleep(10)
+    stop_all()
